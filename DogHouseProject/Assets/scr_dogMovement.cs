70ec3fa7968ac5f5a188bc79c3db63468d71b7cb
mstop4 <mstop4@gmail.com>
2019-01-25 20:09:36 -0500

    basic movement

---
+++ DogHouseProject/Assets/scr_dogMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_dogMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int v_input = 0;
+
+		if (Input.GetKey("up")) v_input += 1;
+		if (Input.GetKey("down")) v_input -= 1;
+
+		transform.Translate(Vector3.forward * Time.deltaTime * v_input);
+	}
+}
