7a41e0c5ddf24f9fa8cac2a084d03bc96590b8a2
Jeremy Andriano <jeremy.andriano3@gmail.com>
2019-01-26 18:41:10 -0500

    barks and feet

---
+++ DogHouseProject/Assets/Scripts/Bark.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bark : MonoBehaviour {
+
+    public AudioSource audio;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audio.Play();
+        }
+
+    }
+}
