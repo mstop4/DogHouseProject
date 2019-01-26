999610c9dd4d505f69fb71c1025d01447771f4ae
mstop4 <mstop4@gmail.com>
2019-01-25 20:52:59 -0500

    added walls

--- DogHouseProject/Assets/scr_dogMovement.cs
+++ DogHouseProject/Assets/scr_dogMovement.cs
@@ -14,11 +14,14 @@ public class scr_dogMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		int v_input = 0;
+		int h_input = 0;
 
 		if (Input.GetKey("up")) v_input += 1;
 		if (Input.GetKey("down")) v_input -= 1;
+		if (Input.GetKey("right")) h_input += 1;
+		if (Input.GetKey("left")) h_input -= 1;
 
-		transform.Translate(Vector3.forward * Time.deltaTime * v_input);
+		transform.Translate((Vector3.forward * v_input + Vector3.right * h_input) * Time.deltaTime * 5);
 
 		if (transform.position.x <= -5 && !arrivedAtGoal) {
 			arrivedAtGoal = true;
