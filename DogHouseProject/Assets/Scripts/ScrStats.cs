9dd6500341fca92cf3670cf12bcf50d9a0fc114e
mstop4 <mstop4@gmail.com>
2019-01-26 15:30:14 -0500

    connected title with main scene

--- DogHouseProject/Assets/Scripts/ScrStats.cs
+++ DogHouseProject/Assets/Scripts/ScrStats.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,10 +11,21 @@ public class ScrStats : MonoBehaviour {
 	public Text timerText;
 	public Text loveText;
 
+	public Text timerLabel;
+	public Text loveLabel;
+
+	public GameObject goalObj;
+	
+	private ScrGoalDetection goalScript;
+
 	// Use this for initialization
 	void Start () {
 		timerText.text = time.ToString();
 		loveText.text = love.ToString();
+		goalScript = goalObj.GetComponent<ScrGoalDetection>();
+
+		loveText.gameObject.SetActive(false);
+		loveLabel.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -23,18 +35,25 @@ public class ScrStats : MonoBehaviour {
 
 			if (time < 0f) {
 				time = 0f;
+				SendMessage("TimeOver");
+				print("Master is back!");
+
+				loveText.gameObject.SetActive(true);
+				loveLabel.gameObject.SetActive(true);
+				timerText.gameObject.SetActive(false);
+				timerLabel.gameObject.SetActive(false);
 			}
-			timerText.text = time.ToString();
+			timerText.text = Math.Round(time, 1).ToString();
 		}
 
 		else {
-			if (love > 0f) {
+			if (!goalScript.hasArrived && love > 0f) {
 				love -= Time.deltaTime;
 
 				if (love < 0f) {
 					love = 0f;
 				}
-				loveText.text = love.ToString();
+				loveText.text = Math.Round(love, 1).ToString();
 			}
 		}
 	}
