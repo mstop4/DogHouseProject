f8f13f44bd91f410f4a99a00e79a5214b696037e
mstop4 <mstop4@gmail.com>
2019-01-26 22:57:51 -0500

    added BGM player

---
+++ DogHouseProject/Assets/Scripts/ScrBGM.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrBGM : MonoBehaviour {
+
+	private AudioSource bgm;
+	private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+		bgm = GetComponent<AudioSource>();
+		bgm.Play(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = mainCamera.transform.position;
+	}
+
+	void onDestroy() {
+		bgm.Stop();
+	}
+}
