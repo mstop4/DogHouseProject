7cf02894566468526f539f88069cee72d1f7eebe
mstop4 <mstop4@gmail.com>
2019-01-26 19:19:06 -0500

    wip2

--- DogHouseProject/Assets/Scripts/ScrOwnerController.cs
+++ DogHouseProject/Assets/Scripts/ScrOwnerController.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class ScrOwnerController : MonoBehaviour {
 
+	private Animator anim;
+
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void IsBack () {
+		anim.isBack = true;
 	}
 }
