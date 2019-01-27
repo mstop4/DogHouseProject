6ac7243a870cc537a427e1e583458089713de6ff
mstop4 <mstop4@gmail.com>
2019-01-26 19:07:31 -0500

    added camera follow

---
+++ DogHouseProject/Assets/Scripts/ScrCameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrCameraFollow : MonoBehaviour {
+	private Camera mainCamera;
+	public Transform t;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mainCamera.transform.position = t.position - (Vector3.left * 2f + Vector3.down) * 3f;
+	}
+}
